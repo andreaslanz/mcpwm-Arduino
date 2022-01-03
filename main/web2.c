664dfd7ff0db07cbc6ffe8ac33615c66234f5443
al <andi.lanz@gmail.com>
2022-01-03 07:49:49 +0100

    remove ctrl_put_handler

--- main/web2.c
+++ main/web2.c
@@ -303,45 +303,7 @@ esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
 /* An HTTP PUT handler. This demonstrates realtime
  * registration and deregistration of URI handlers
  */
-static esp_err_t ctrl_put_handler(httpd_req_t *req)
-{
-    char buf;
-    int ret;
 
-    if ((ret = httpd_req_recv(req, &buf, 1)) <= 0) {
-        if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
-            httpd_resp_send_408(req);
-        }
-        return ESP_FAIL;
-    }
-
-    if (buf == '0') {
-        /* URI handlers can be unregistered using the uri string */
-        ESP_LOGI(TAG, "Unregistering /hello and /echo URIs");
-        httpd_unregister_uri(req->handle, "/hello");
-        httpd_unregister_uri(req->handle, "/echo");
-        /* Register the custom error handler */
-        httpd_register_err_handler(req->handle, HTTPD_404_NOT_FOUND, http_404_error_handler);
-    }
-    else {
-        ESP_LOGI(TAG, "Registering /hello and /echo URIs");
-        httpd_register_uri_handler(req->handle, &hello);
-        httpd_register_uri_handler(req->handle, &echo);
-        /* Unregister custom error handler */
-        httpd_register_err_handler(req->handle, HTTPD_404_NOT_FOUND, NULL);
-    }
-
-    /* Respond with empty body */
-    httpd_resp_send(req, NULL, 0);
-    return ESP_OK;
-}
-
-static const httpd_uri_t ctrl = {
-        .uri       = "/ctrl",
-        .method    = HTTP_PUT,
-        .handler   = ctrl_put_handler,
-        .user_ctx  = NULL
-};
 
 static httpd_handle_t start_webserver(void)
 {
@@ -370,7 +332,6 @@ static httpd_handle_t start_webserver(void)
         httpd_register_uri_handler(server, &zahl);
         httpd_register_uri_handler(server, &hello);
         httpd_register_uri_handler(server, &echo);
-        httpd_register_uri_handler(server, &ctrl);
         return server;
     }
 
